6545b4442bd4234e750c533bcf8109dcbb2fc166
C-Macargo <caio.camargos97@gmail.com>
2023-01-12 17:18:35 -0300

    connect mongo

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
-
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
 import express from "express";
 import cors from "cors";
 
+dotenv.config();
 const PORT = 5000;
-
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+const db = mongoClient.db();
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);});
+async function startServer() {
+  try {
+    await mongoClient.connect();
+    console.log("MongoDB Connected!");
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
+}
+startServer();
